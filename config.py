4f2ddb16ea2ed62afd5c09be6f6f4a08061faeca
Sadnesspotato <163751247+Sadnesspotato@users.noreply.github.com>
2024-04-16 12:51:17 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-2.czmk0i2ycwk4.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Aacs3253"
+customdb = "employee"
+custombucket = "tangwaizhong-employee"
+customregion = "us-east-1"
+
